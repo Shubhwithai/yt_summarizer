1114bdb841fb3fdf7c63e5cd435bc466eabbe77d
Shubhwithai <shubham@buildfastwithai.com>
2025-02-08 23:16:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api._errors import TranscriptsDisabled, NoTranscriptAvailable
+import streamlit as st
 
 def is_valid_youtube_url(url):
     """Check if the URL is a valid YouTube URL."""
